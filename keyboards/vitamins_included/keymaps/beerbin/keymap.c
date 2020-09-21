46a087dbae691d1cc5f0f960f84572cb2ddcaac4
beerbin <ryan.karpinski2@gmail.com>
2020-09-20 20:27:37 -0500

    updated layout

--- keyboards/vitamins_included/keymaps/beerbin/keymap.c
+++ keyboards/vitamins_included/keymaps/beerbin/keymap.c
@@ -81,20 +81,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Gaming
  * ,-----------------------------------------------------------------------------------.
- * | Esc  |   6  |   7  |   8  |   9  |   0  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Tab  |   1  |   2  |   3  |   4  |   5  |   H  |   J  |   K  |   L  |   ;  |  '   |
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  T   |  F1  |  F2  |  F3  |  F4  |  F5  |   B  |   M  |   ,  |   .  |   /  |Enter |
+ * | F8   |   Z  |   X  |   C  |   V  |   B  |   B  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |   R  |   F  | Lower|Space |   V  |Space |Raise | Left | Down |  Up  |Right |
+ * | Ctrl |   F9 |  ALT | Lower|Space |  F7  |Space |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_GAMING] = LAYOUT_ortho_4x12( \
-  KC_GESC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_T,	   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_B,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  KC_LCTRL,KC_R,    KC_F,    KC_F6,   KC_SPC,  KC_V,    KC_SPC,  UP_HYP,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+  KC_LCTRL,KC_F9,   KC_LALT, KC_F6,   KC_SPC,  KC_F7,   KC_SPC,  UP_HYP,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Dvorak
@@ -148,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, RESET, \
-  DF(0),   DF(1),   DF(2),   _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+  DF(0),   DF(1),   DF(2),   _______, _______, _______, _______, _______, KC_HOME, KC_VOLD, KC_VOLU, KC_END \
 ),
 
 };
